87e130d43ea4ff634efbc4c8068bec7728bb6315
RizkiRb <rizkibinyola25@gmail.com>
2023-10-24 07:59:13 +0700

    kintil

--- panel/dashboard/data-pendaftaran.php
+++ panel/dashboard/data-pendaftaran.php
@@ -1,89 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Data Pendaftaran - WebKolah</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <!-- Navbar -->
-        <?php include 'navbar.php'; ?>
-        
-            <div id="layoutSidenav_content">
-                <!-- Start Body Content -->
-                <main>
-                    <!-- Body Content -->
-                    <div class="container">
-                        <h3 class="text-secondary display-6">Data Pendaftaran</h3>
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Data Pendaftaran</li>
-                            </ol>
-                        </nav>
-                    </div>
-                    <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                DataTable Example
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>nis</th>
-                                            <th>nama_siswa</th>
-                                            <th>alamat</th>
-                                            <th>jenis kelamin</th>
-                                            <th>Detail</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                            include 'koneksi/koneksi.php';
-                                            $no = 1;
-                                            $query = "SELECT *
-                                            FROM pendaftaran ";
-                                            $sql = mysqli_query($conn, $query);
-                                            while ($row = mysqli_fetch_assoc($sql)) {
-                                        ?>
-                                        <tr>
-                                            <td><?= $no++; ?></td>
-                                            <td><?= $row['nis'] ?></td>
-                                            <td><?= $row['nama_siswa']?></td>
-                                            <td><?= $row['alamat']?></td>
-                                            <td><?= $row['jenis_kelamin']?></td>
-                                            <td><a class="btn btn-warning btn-sm" type="button"></a></td>
-                                            <td><a class="btn btn-warning btn-sm" type="button"></a></td>
-                                        </tr>
-                                        <?php
-                                            }
-                                        ?>
 
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                </main>
-                <!-- End Body Content -->
-            <?php include 'footer.php'; ?>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Data Pendaftaran - WebKolah</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <!-- Navbar -->
+    <?php include 'navbar.php'; ?>
+
+    <div id="layoutSidenav_content">
+        <!-- Start Body Content -->
+        <main>
+            <!-- Body Content -->
+            <div class="container">
+                <h3 class="text-secondary display-6">Data Pendaftaran</h3>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data Pendaftaran</li>
+                    </ol>
+                </nav>
             </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
-    
-    </body>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    DataTable Example
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>nis</th>
+                                <th>nama_siswa</th>
+                                <th>alamat</th>
+                                <th>jenis kelamin</th>
+                                <th>Detail</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include 'koneksi/koneksi.php';
+                            $no = 1;
+                            $query = "SELECT *
+                                            FROM pendaftaran ";
+                            $sql = mysqli_query($conn, $query);
+                            while ($row = mysqli_fetch_assoc($sql)) {
+                            ?>
+                                <tr>
+                                    <td><?= $no++; ?></td>
+                                    <td><?= $row['nis'] ?></td>
+                                    <td><?= $row['nama_siswa'] ?></td>
+                                    <td><?= $row['alamat'] ?></td>
+                                    <td><?= $row['jenis_kelamin'] ?></td>
+                                    <td><!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            
+                                        </button>
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        ...
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td><a class="btn btn-warning btn-sm" type="button"></a></td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+        <!-- End Body Content -->
+        <?php include 'footer.php'; ?>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+
+</body>
+
 </html>
